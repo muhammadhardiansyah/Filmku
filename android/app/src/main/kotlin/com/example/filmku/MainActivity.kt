c55180ceb803e37d39bc687e630fc4ae860e4ed5
Muhammad Hardiansyah <71612996+muhammadhardiansyah@users.noreply.github.com>
2022-12-17 06:32:02 +0700

    filmku

---
+++ android/app/src/main/kotlin/com/example/filmku/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.filmku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:filmku/style/theme.dart' as Style;
+import 'package:filmku/widgets/best_movies.dart';
+import 'package:filmku/widgets/genres.dart';
+import 'package:filmku/widgets/now_playing.dart';
+import 'package:filmku/widgets/persons.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Style.Colors.mainColor,
+      appBar: AppBar(
+        backgroundColor: Style.Colors.mainColor,
+        centerTitle: true,
+        leading: Icon(EvaIcons.menu2Outline, color: Colors.white,),
+        title: Text("Filmku"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(EvaIcons.searchOutline, color: Colors.white,)
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          NowPlaying(),
+          GenresScreen(),
+          //PersonsList(),
+          BestMovies(),
+        ],
+      ),
+    );
+  }
+}
